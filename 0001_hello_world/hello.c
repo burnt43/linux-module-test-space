b49ec689df1037cfd99881e1c6aa7da8f9dc9600
burnt43 <jms.crsn@gmail.com>
2015-11-28 23:57:48 +0000

    adding parameters to the module

--- 0001_hello_world/hello.c
+++ 0001_hello_world/hello.c
@@ -1,14 +1,25 @@
 #include<linux/init.h>
 #include<linux/module.h>
 #include<linux/version.h>
+#include<linux/moduleparam.h>
 MODULE_LICENSE("Dual BSD/GPL");
 
+static char *whom = "world";
+static int howmany = 1;
+
+module_param(howmany, int, S_IRUGO);
+module_param(whom, charp, S_IRUGO);
+
 static int hello_init (void) {
-  printk(KERN_ALERT "Hello, World! kernel version: %d.%d.%d\n",
+  int i;
+  printk(KERN_ALERT "kernel version: %d.%d.%d\n",
     (LINUX_VERSION_CODE & 0XFF0000) >> 16,
     (LINUX_VERSION_CODE & 0x00FF00) >> 8,
     (LINUX_VERSION_CODE & 0x0000FF)
   );
+  for (i = 0; i < howmany; i++) {
+    printk(KERN_ALERT "Hello, %s\n",whom);
+  }
   return 0;
 }
 
