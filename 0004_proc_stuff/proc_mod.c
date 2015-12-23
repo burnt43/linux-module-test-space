49ad7f195d224b830b845d672661b488421fa0f0
burnt43 <jms.crsn@gmail.com>
2015-12-23 04:09:54 +0000

    tried using new proc stuff. broke the kernel

--- 0004_proc_stuff/proc_mod.c
+++ 0004_proc_stuff/proc_mod.c
@@ -1,9 +1,36 @@
 #include<linux/module.h>
 #include<linux/kernel.h>
 #include<linux/proc_fs.h>
+#include<linux/seq_file.h>
 
-#define procfs_name "helloworld"
+#define mod_name "proc_test"
 
+static int my_proc_open (struct inode *, struct file *);
+
+static struct file_operations fops = {
+  .owner   = THIS_MODULE,
+  .open    = my_proc_open,
+  .llseek  = seq_lseek,
+  .read    = seq_read,
+  .release = single_release
+};
+
+static int my_proc_open (struct inode *inode, struct file *file) {
+  printk(KERN_INFO "%s: open\n", mod_name);
+  return 0;
+}
+
+int init_module () {
+  printk(KERN_INFO "%s: starting up\n", mod_name);
+  proc_create_data(mod_name, 0, NULL, &fops, NULL);
+  return 0;
+}
+
+void cleanup_module () {
+  printk(KERN_INFO "%s: cleaning up\n", mod_name);
+  remove_proc_entry(mod_name, NULL);
+}
+/*
 struct proc_dir_entry *our_proc_file;
 
 int procfile_read (char *buffer, char **buffer_location, off_t offset, int buffer_length, int *eof, void *data) {
@@ -41,3 +68,4 @@ void cleanup_module () {
   remove_proc_entry(procfs_name, &proc_root);
   printk(KERN_INFO "%s: proc_file removed\n", procfs_name);
 }
+*/
