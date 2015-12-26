d6b8508c9fde5c22f65d4719a38df3d53eef60ba
burnt43 <jms.crsn@gmail.com>
2015-12-26 00:46:41 +0000

    add debug to inode structure

--- 0003_char_dev/char_dev_mod_0000.c
+++ 0003_char_dev/char_dev_mod_0000.c
@@ -9,6 +9,7 @@ static int device_open(struct inode *, struct file *);
 static int device_release(struct inode *, struct file *);
 static ssize_t device_read(struct file *, char *, size_t, loff_t *);
 static ssize_t device_write(struct file *, const char *, size_t, loff_t *);
+void print_flag_data(const char *, unsigned long, const char *, unsigned long);
 void debug_inode(struct inode *);
 
 MODULE_LICENSE("GPL");
@@ -52,11 +53,24 @@ void cleanup_module(void) {
   unregister_chrdev(major,DEVICE_NAME);
 }
 
+void print_flag_data(const char *flag_name, unsigned long flag_value, const char *bits_name, unsigned long bits_value) {
+  printk(KERN_INFO "%s: %5lu & %lu (\033[0;35m%s\033[0;39m & %s) = %s\n", DEVICE_NAME, flag_value, bits_value, flag_name, bits_name, (flag_value & bits_value) > 0 ? "\033[0;32m1\033[0;39m" : "\033[0;31m0\033[0;39m");
+}
+
+#define I_MODE_LENGTH 12
+#define I_OPFLAG_LENGTH 3
 void debug_inode(struct inode *inode) {
   umode_t tmp_umode_result = 0; 
   char *tmp_message;
+  static char* i_mode_names[I_MODE_LENGTH]    = {"S_ISUID", "S_ISGID", "S_ISVTX", "S_IRUSR", "S_IWUSR", "S_IXUSR", "S_IRGRP", "S_IWGRP", "S_IXGRP", "S_IROTH", "S_IWOTH", "S_IXOTH"};
+  static umode_t i_mode_values[I_MODE_LENGTH] = {S_ISUID, S_ISGID, S_ISVTX, S_IRUSR, S_IWUSR, S_IXUSR, S_IRGRP, S_IWGRP, S_IXGRP, S_IROTH, S_IWOTH, S_IXOTH};
+
+  static char* i_opflag_names[I_OPFLAG_LENGTH]           = {"IOP_FASTPERM", "IOP_LOOKUP", "IOP_NOFOLLOW"};
+  static unsigned short i_opflag_values[I_OPFLAG_LENGTH] = {IOP_FASTPERM, IOP_LOOKUP, IOP_NOFOLLOW};
+  int index = 0;
 
   printk(KERN_INFO "%s: --------------------------INODE DEBUG----------------------------\n", DEVICE_NAME);
+  // umode_t i_mode : says what kind of file it is and what permissions it has
   printk(KERN_INFO "%s: \033[0;31minode->i_mode\033[0;39m(umode_t): %d\n", DEVICE_NAME, inode->i_mode);
   tmp_umode_result = S_IFMT & inode->i_mode;
   switch ( tmp_umode_result ) {
@@ -84,7 +98,15 @@ void debug_inode(struct inode *inode) {
     default:
       tmp_message = "unknown";
   }
-  printk(KERN_INFO "%s: %d & %d (S_IFMT & inode->i_mode) = %d(%s)", DEVICE_NAME, S_IFMT, inode->i_mode, tmp_umode_result, tmp_message);
+  printk(KERN_INFO "%s: %d & %d (\033[0;35mS_IFMT\033[0;39m & inode->i_mode) = %d(\033[0;35m%s\033[0;39m)", DEVICE_NAME, S_IFMT, inode->i_mode, tmp_umode_result, tmp_message);
+  for (index=0; index < I_MODE_LENGTH; index++) {
+    print_flag_data(i_mode_names[index], i_mode_values[index], "i_mode", inode->i_mode);
+  }
+  // unsigned short i_opflags : not sure what this stuff means
+  printk(KERN_INFO "%s: \033[0;31minode->i_opflags\033[0;39m(unsigned short): %d\n", DEVICE_NAME, inode->i_opflags);
+  for (index=0; index < I_OPFLAG_LENGTH; index++) {
+    print_flag_data(i_opflag_names[index], i_opflag_values[index], "i_opflags", inode->i_opflags);
+  }
   printk(KERN_INFO "%s: -----------------------------------------------------------------\n", DEVICE_NAME);
 }
 
